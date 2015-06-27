9b1bcf1b5bb70efb775168ab0ea55dcf30706ee4
Kaitlyn Dodds <kzd5123@psu.edu>
2015-06-27 14:59:13 -0700

    External testing reset

--- config.inc.php
+++ config.inc.php
@@ -1,19 +1,19 @@
 <?php
 // External testing
-/*
+
 define('DB_HOST', 'us-cdbr-azure-east-b.cloudapp.net');
 define('DB_NAME', 'psueyetstore');
 define('DB_USERNAME','bac616bfae3054');
 define('DB_PASSWORD','40b248a7');
-*/
+
 
 // Local testing
-// DB_NAME = negative_options
+/*
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'psueyetstore');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-
+*/
 define('USE_MTURK', false);
 
 define('SAFEDELIVERY_UNAME',"safedeliveryservice1@gmail.com");
